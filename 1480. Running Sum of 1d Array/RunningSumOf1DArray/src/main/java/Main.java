db7245f5061ed0701a9b7ec53fa42ccfa1ef635d
prosauce86 <prosauce86@gmail.com>
2023-08-07 17:24:12 -0400

    working accepted solution

--- 1480. Running Sum of 1d Array/RunningSumOf1DArray/src/main/java/Main.java
+++ 1480. Running Sum of 1d Array/RunningSumOf1DArray/src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(new Solution().runningSum(new int[]{1,2,3,4})));
+        System.out.println();
+        System.out.println(Arrays.toString(new Solution().runningSum(new int[]{1,1,1,1,1})));
+        System.out.println();
+        System.out.println(Arrays.toString(new Solution().runningSum(new int[]{3,1,2,10,1})));
     }
 }
 
 class Solution {
-    
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
 }
