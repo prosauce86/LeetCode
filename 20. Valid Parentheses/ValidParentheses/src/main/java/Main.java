55ecaff307c461094e42c05b3e02c0dd71abdf82
prosauce86 <prosauce86@gmail.com>
2023-08-07 16:17:11 -0400

    working accepted code to be merged to master

--- 20. Valid Parentheses/ValidParentheses/src/main/java/Main.java
+++ 20. Valid Parentheses/ValidParentheses/src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().isValid("()[]{}"));
     }
 }
 
 class Solution {
-    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> charMap = new HashMap<>();
+        charMap.put(')', '(');
+        charMap.put(']', '[');
+        charMap.put('}', '{');
+        for (Character c : s.toCharArray()) {
+            if (charMap.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek().equals(charMap.get(c))) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
 }
